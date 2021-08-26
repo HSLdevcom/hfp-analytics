629d18845b42af057f3f74f6015b23c5601fd681
arttu.kosonen <arttu.kosonen@hsl.fi>
2021-08-26 17:26:02 +0300

    Create db test setup

---
+++ test-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function cleanup {
+  docker-compose -f docker-compose.test.server.yml down
+}
+
+trap cleanup EXIT
+
+cp .env.test .env && \
+  source .env && \
+  docker-compose -f docker-compose.test.server.yml up
