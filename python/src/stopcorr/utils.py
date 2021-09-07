8469826f064639410e8da918d5470a905e50da70
arttu.kosonen <arttu.kosonen@hsl.fi>
2021-09-07 08:45:26 +0300

    Add analyze_pptx

--- python/src/stopcorr/utils.py
+++ python/src/stopcorr/utils.py
@@ -27,3 +27,28 @@ def comma_separated_integers_to_list(val_str):
     res = [x.strip() for x in res]
     res = [int(x) for x in res]
     return res
+
+# From https://pbpython.com/creating-powerpoint.html
+def analyze_pptx(input_pptx, output_pptx):
+    """ Take the input file and analyze the structure.
+    The output file contains marked up information to make it easier
+    for generating future powerpoint templates.
+    """
+    prs = pptx.Presentation(input_pptx)
+    for index, _ in enumerate(prs.slide_layouts):
+        slide = prs.slides.add_slide(prs.slide_layouts[index])
+        try:
+            title = slide.shapes.title
+            title.text = 'Title for Layout {}'.format(index)
+        except AttributeError:
+            print("No Title for Layout {}".format(index))
+        for shape in slide.placeholders:
+            if shape.is_placeholder:
+                phf = shape.placeholder_format
+                try:
+                    if 'Title' not in shape.text:
+                        shape.text = 'Placeholder index:{} type:{}'.format(phf.idx, shape.name)
+                except AttributeError:
+                    print("{} has no text attribute".format(phf.type))
+                print('{} {}'.format(phf.idx, shape.name))
+    prs.save(output_pptx)
