861cc7d443d68873e846a14c7155e40b3cfc5246
arttu.kosonen <arttu.kosonen@hsl.fi>
2021-09-07 09:50:02 +0300

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
+# Sets up the project from a newly cloned repo.
 
 set -euo pipefail
 
-if [[ -e .env ]]; then
-  read -p ".env already exists, overwrite? [y/n] " answer
-  if [[ "$answer" == "y" ]]; then
-    echo "Overwriting .env"
-    cp .env.test .env
+function conditional_cp () {
+  if [[ -e "$2" ]]; then
+    read -p "$2 already exists, overwrite? [y/n] " answer
+    if [[ "$answer" == "y" ]]; then
+      cp "$1" "$2"
+      echo "$2 overwritten from $1"
+    fi
+  else
+    cp "$1" "$2"
+    echo "$2 created from $1"
   fi
-else
-  cp .env.test .env
-  echo ".env created from .env.test; adjust to your needs"
-fi
+}
 
-mkdir -p data
-mkdir -p data/import
-docker network rm stopcorr_nw || true > /dev/null 2>&1
-docker network create --attachable stopcorr_nw
+function conditional_mkdir () {
+  if [[ -d "$1" ]]; then
+    echo "$1 directory already exists"
+  else
+    mkdir -p "$1"
+    echo "$1 directory created"
+  fi
+}
+
+conditional_cp docker-compose.test.yml docker-compose.yml
+conditional_cp .env.test .env
+conditional_mkdir data/import
+conditional_mkdir qgis/out
+conditional_mkdir results
+
+if [ ! "$(docker network ls | grep stopcorr_nw)" ]; then
+  docker network create --attachable stopcorr_nw
+  echo "stopcorr_nw docker network created"
+else
+  echo "stopcorr_nw docker network already exists"
+fi
