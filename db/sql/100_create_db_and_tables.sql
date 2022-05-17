6bf61ca9caf3effa14cc0b9b61d90d87f152f308
datarttu <arttu.kosonen@gmail.com>
2022-05-17 13:31:58 +0300

    Add comment string

--- db/sql/100_create_db_and_tables.sql
+++ db/sql/100_create_db_and_tables.sql
@@ -21,6 +21,12 @@ RETURNS anyarray
 AS $f$
   SELECT array_agg(DISTINCT x) FROM unnest($1) t(x);
 $f$ LANGUAGE SQL IMMUTABLE;
+COMMENT ON FUNCTION array_distinct IS
+'Eliminates any duplicates from an array. E.g.
+ > SELECT array_distinct_notnull(array[1, 2, 2]);
+  array_distinct
+═════════════════
+  {1,2}';
 
 --
 -- HFP observations
