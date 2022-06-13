b834c964887bdc5f4ae2558f58ba957f85538ab1
niemijoe <joe.niemi@cgi.com>
2022-06-13 15:31:12 +0300

    Timezone setting, fixed importer from getting crashed if loading of one blob fails

--- deploy_api.sh
+++ deploy_api.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eu
+
 az acr login --name hfpanalyticsregistry
 
 docker build -t hfpanalyticsregistry.azurecr.io/hfp-analytics/api:dev -f Dockerfile.api_deploy .
--- deploy_importer.sh
+++ deploy_importer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eu
+
 az acr login --name hfpanalyticsregistry
 
 docker build -t hfpanalyticsregistry.azurecr.io/hfp-analytics/importer:dev -f Dockerfile.importer_deploy .
