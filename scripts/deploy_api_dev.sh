e6e9222b885b7d9681595b591188e4657c777175
niemijoe <joe.niemi@cgi.com>
2022-10-11 12:09:05 +0300

    Remove deploy scripts

--- scripts/deploy_api_dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-az acr login --name hfpanalyticsregistry
-
-docker build -t hfpanalyticsregistry.azurecr.io/hfp-analytics/api:dev -f ../Dockerfile.api_deploy ../
-
-docker push hfpanalyticsregistry.azurecr.io/hfp-analytics/api:dev
--- scripts/deploy_api_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-az acr login --name hfpanalyticsregistry
-
-docker build -t hfpanalyticsregistry.azurecr.io/hfp-analytics/api:test -f ../Dockerfile.api_deploy ../
-
-docker push hfpanalyticsregistry.azurecr.io/hfp-analytics/api:test
