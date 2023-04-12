030d71d19561ca6576870b41f3bdf09f68d26d32
Juho Hänninen <juho.hanninen@cgi.com>
2023-04-12 14:26:18 +0300

    Get slack envs from config.py

--- python/common/slack.py
+++ python/common/slack.py
@@ -1,24 +1,19 @@
 import requests
-import os
 
-SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL", "")
-SLACK_USERS_TO_ALERT = os.getenv("SLACK_USERS_TO_ALERT", "")
+from common.config import SLACK_WEBHOOK_URL, SLACK_USERS_TO_ALERT
+
 
 # Call this function to send messages to Slack channel. Use alert=True to tag users.
 def send_to_channel(message, alert=False):
+    if not SLACK_WEBHOOK_URL:
+        # Slack url not found, exit
+        return
 
-  if not SLACK_WEBHOOK_URL:
-    # Slack url not found, exit
-    return
+    # Parse users and convert them to slack tag format
+    alert_msg = ""
+    if alert and len(SLACK_USERS_TO_ALERT) > 0:
+        alert_list = ", ".join([f"<@{u}>" for u in SLACK_USERS_TO_ALERT])
+        alert_msg = f"ALERT {alert_list}: "
 
-  # Parse users and convert them to slack tag format
-  alert_msg = ""
-  if alert and SLACK_USERS_TO_ALERT:
-    users = SLACK_USERS_TO_ALERT.split(",")
-    alert_list = ", ".join([f"<@{u}>" for u in users])
-    alert_msg = f"ALERT {alert_list}: "
-
-  msg_object = {
-    "text": f"{alert_msg}{message}"
-  }
-  requests.post(SLACK_WEBHOOK_URL, json=msg_object)
+    msg_object = {"text": f"{alert_msg}{message}"}
+    requests.post(SLACK_WEBHOOK_URL, json=msg_object)
