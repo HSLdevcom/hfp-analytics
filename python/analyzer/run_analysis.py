78815031554ebd059e41a5854d700dffa04387cf
Anton Jyrkiäinen <anton.jyrkiainen@helsinki.fi>
2023-06-05 16:24:44 +0300

    Revert "make sure prev day analysis is done for prev day"
    This reverts commit ae61e4acb23285e063496d6bef272d4c529a87e7.

--- python/analyzer/run_analysis.py
+++ python/analyzer/run_analysis.py
@@ -30,10 +30,7 @@ logger = logging.getLogger('importer')
 
 async def run_vehicle_analysis():
     today = date.today()
-    # Situations where the analysis is run before midnight causes the date for "yesterday" to be the day before yesterday
-    # We're adding 6 hours to current datetime to make sure the date for "yesterday" is yesterday from next morning's perspective
-    today_plus_6_hours = today + timedelta(hours=6)
-    yesterday = today_plus_6_hours - timedelta(days=1)
+    yesterday = today - timedelta(days=1)
     logger.info(f"Starting vehicle analysis for day {yesterday}.")
     vehicles = await get_vehicle_ids(yesterday)
     logger.info(f"Vehicle ids fetched: {vehicles}")
