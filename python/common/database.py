e2472e8f2e21f44cd8086e78f2a1fd2950c22857
Anton Jyrkiäinen <anton.jyrkiainen@helsinki.fi>
2025-05-22 09:19:24 +0300

    testing some memory improvements

--- python/common/database.py
+++ python/common/database.py
@@ -1,4 +1,18 @@
 from psycopg_pool import AsyncConnectionPool
 from common.config import POSTGRES_CONNECTION_STRING
 
-pool = AsyncConnectionPool(POSTGRES_CONNECTION_STRING, max_size=20)
+class _AsyncPool:
+    def __init__(self, conn_string: str, max_size: int):
+        self._conn_string = conn_string
+        self._max_size = max_size
+        self._pool: AsyncConnectionPool | None = None
+
+    def _ensure_pool(self):
+        if self._pool is None:
+            self._pool = AsyncConnectionPool(self._conn_string, max_size=self._max_size)
+
+    def __getattr__(self, name):
+        self._ensure_pool()
+        return getattr(self._pool, name)
+
+pool = _AsyncPool(POSTGRES_CONNECTION_STRING, max_size=20)
