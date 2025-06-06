5f5f38f3e3ec605157aa0972a95ce33aceb47d03
Damian Wysokinski <damian.wysokinski@nordcloud.com>
2025-06-06 10:47:29 +0200

    added messaging for is_date_range_valid is_date_range_valid

--- python/api/routers/hfp.py
+++ python/api/routers/hfp.py
@@ -491,12 +491,12 @@ async def get_delay_analytics_data(
 
         if (to_oday is None):
             to_oday = default_to_oday
-            
-        if not is_date_range_valid(start_date=from_oday, end_date=to_oday):
-            raise HTTPException(
-                status_code=422,
-                detail="Incorrect date range. Between 'from_oday' and 'to_oday' should be max 7 weeks (49 days). 'to_oday' is inclusive."
-            )
+        
+        is_date_range_valid_, date_range_validity_message = is_date_range_valid(
+            from_oday=from_oday, to_oday=to_oday
+        )
+        if not is_date_range_valid_:
+            raise HTTPException(status_code=422, detail=date_range_validity_message)
 
         if route_id is None or not route_id.strip():
             route_ids = "ALL"
